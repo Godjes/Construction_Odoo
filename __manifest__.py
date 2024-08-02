9c37562ad903f2cfda25bfb436fe76236fc7af58
Muhamadeev Ramil <Ramil.mukhamadeev@mail.ru>
2024-08-02 11:00:01 +0500

    Добавлен виджет для поля phone и mobile в модуле контакты, добалвен видже o2manyfield

--- __manifest__.py
+++ __manifest__.py
@@ -3,9 +3,11 @@
     'name': "Construction",
     'description': """Для отчета о проделанной работе""",
     'author': "Rydlab",
+    'summary': 'Construction widget',
     'website': "https://rydlab.ru/",
     'category': 'Construction',
     'version': '16.0.0.3.1',
+    'sequence': -2,
     'depends': ['base', 'mail', 'contacts', 'product', 'stock', 'fleet'],
     'data': [
         #'security/groups.xml',
@@ -17,6 +19,14 @@
         'views/construction_category.xml',
         'views/construction_work.xml',
         'views/construction_objects.xml',
-        'views/stock.xml'
+        'views/stock.xml',
+        'views/res_parnter.xml'
     ],
+    'assets': {
+        'web.assets_backend': [
+            'construction/static/src/components/*/*.js',
+            'construction/static/src/components/*/*.xml',
+            'construction/static/src/components/*/*.scss',
+        ],
+    },
 }
---
+++ static/src/components/form_mobile_field/form_telegram_field.js
@@ -0,0 +1,27 @@
+/** @odoo-module */
+
+import { registry } from "@web/core/registry"
+import { PhoneField } from "@web/views/fields/phone/phone_field";
+
+class FormTelegramField extends PhoneField {
+    setup(){
+        super.setup()
+    }
+
+    get telegramHref() {
+        const phoneNumber = this.props.value.replace(/\D/g, '');
+        if (phoneNumber.startsWith('7')) {
+            return `https://t.me/+${phoneNumber}`;
+        } else if (phoneNumber.startsWith('8')) {
+            return `https://t.me/+7${phoneNumber.slice(1)}`;
+        } else if (phoneNumber.length === 10) {
+            return `https://t.me/+7${phoneNumber}`;
+        } else {
+            return '';
+        }
+    }
+}
+
+FormTelegramField.template = "owl.FormTelegramField"
+
+registry.category("fields").add("telegram_mobile", FormTelegramField)
